a369e5142c74dc91d1e9c7a5e134af477dfc7144
alieveldar <illuminatiday@gmail.com>
2019-02-22 15:48:49 +0300

    Adding order.js

--- assets/js/order.js
+++ assets/js/order.js
@@ -4,8 +4,11 @@ function() {
         var price = $("#price");
         var summ = $("#summ");
         var discountp = $("#discount");
+        pricenum = price.text().split("Цена: ");
         if ((Number(discount.text()) < 100 && (Number(discount.text())) > 0)) {
-            summ.text(Number(price.text()) * (Number(discount.text()) / 100))
+            var temp = (pricenum[1] - (pricenum[1] * (Number(discount.text()) / 100)));
+            summ.text(temp + " руб.");
+            price.css('textDecoration', 'line-through');
         } else {
             discountp.hide();
             summ.hide();
@@ -13,6 +16,7 @@ function() {
 
         var buttond = $(".buttond");
         buttond.on("click", function {
+            alert("OK");
             var bar = $("#orderBar");
             var width = 1;
             var id = setInterval(frame, 10);
