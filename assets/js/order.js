0196e75b335f7959cdfb640c02dd4100d6c41337
alieveldar <illuminatiday@gmail.com>
2019-02-22 21:52:44 +0300

    Test js progress

--- assets/js/order.js
+++ assets/js/order.js
@@ -1,32 +1,33 @@
 $(document).ready(function() {
-        var discount = $("#discountval");
-        var price = $("#price");
-        var summ = $("#summ");
-        var discountp = $("#discount");
-        pricenum = price.text().split("Цена: ");
-        if ((Number(discount.text()) < 100 && (Number(discount.text())) > 0)) {
-            var temp = (pricenum[1] - (pricenum[1] * (Number(discount.text()) / 100)));
-            summ.text(temp + " руб.");
-            price.css('textDecoration', 'line-through');
-        } else {
-            discountp.hide();
-            summ.hide();
-        }
+    var discount = $("#discountval");
+    var price = $("#price");
+    var summ = $("#summ");
+    var discountp = $("#discount");
+    pricenum = price.text().split("Цена: ");
+    if ((Number(discount.text()) < 100 && (Number(discount.text())) > 0)) {
+        var temp = (pricenum[1] - (pricenum[1] * (Number(discount.text()) / 100)));
+        summ.text(temp + " руб.");
+        price.css('textDecoration', 'line-through');
+    } else {
+        discountp.hide();
+        summ.hide();
+    }
 
-        var buttond = $(".buttond");
-        buttond.on("click", function() {
-            alert("OK");
-            var bar = $("#orderBar");
-            var width = 1;
-            var id = setInterval(frame, 10);
-            if (width >= 100) {
-                clearInterval(id);
-            } else {
+    var buttond = $(".buttond");
+    buttond.on("click", function() {
+        alert("OK");
+        var bar = $("#orderBar");
+        var width = 1;
+        if (width >= 100) {
+            width = 1;
+        } else {
+            while (width < 100) {
                 console.log(width + 1);
                 width++;
                 bar.css('width', width);
             }
-
-        });
+        }
 
     });
+
+});
