f4bb388450deaa53f9155722982e80ab97fdf1b3
alieveldar <illuminatiday@gmail.com>
2019-02-22 14:41:56 +0300

    Add progress bar2 js

--- assets/js/order.js
+++ assets/js/order.js
@@ -1,16 +1,31 @@
-function(){
-$( document ).ready(function() {
-    var discount = $("#discountval");
-    var price = $("#price");
-    var summ = $("#summ");
-    var discountp = $("#discount");
-    if((Number(discount.text()) < 100 && (Number(discount.text())) > 0)) {
-    	summ.text(Number(price.text()) * (Number(discount.text()) / 100))
-    }
-    else {
-    	discountp.hide();
-    	summ.hide();
-    }
+function() {
+    $(document).ready(function() {
+        var discount = $("#discountval");
+        var price = $("#price");
+        var summ = $("#summ");
+        var discountp = $("#discount");
+        if ((Number(discount.text()) < 100 && (Number(discount.text())) > 0)) {
+            summ.text(Number(price.text()) * (Number(discount.text()) / 100))
+        } else {
+            discountp.hide();
+            summ.hide();
+        }
 
-});
+        var buttond = $(".buttond");
+        buttond.on("click", function {
+            var bar = $("#orderBar");
+            var width = 1;
+            var id = setInterval(frame, 10);
+
+            function frame() {
+                if (width >= 100) {
+                    clearInterval(id);
+                } else {
+                    width++;
+                    elem.style.width = width + '%';
+                }
+            }
+        });
+
+    });
 }
