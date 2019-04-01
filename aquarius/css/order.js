a22d280700ec3277f8de81e3a6704313bb4eb7f6
alieveldar <illuminatiday@gmail.com>
2019-04-01 13:21:45 +0300

    add js to order

---
+++ aquarius/css/order.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+var but = $(".t780__btn-wrapper");
+var buttonhref = $(".t780__btn t-btn");
+var color = $(".t-product__option-select t-descr t-descr_xxs js-product-option-variants").val();
+var tel = $(".tel").val();
+but.mouseover(function(){
+  buttonhref..attr("href", "https://order.privator.club/aquarius.php?color="+ color +"&tel=" + tel;
+});
+}
