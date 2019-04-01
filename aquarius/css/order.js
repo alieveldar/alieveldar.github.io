6dd9d2a8f5730b9ff357620daa39ef6664456d1a
alieveldar <illuminatiday@gmail.com>
2019-04-01 13:26:54 +0300

    fix2

--- aquarius/css/order.js
+++ aquarius/css/order.js
@@ -4,6 +4,6 @@ var buttonhref = $(".t780__btn t-btn");
 var color = $(".t-product__option-select t-descr t-descr_xxs js-product-option-variants").val();
 var tel = $(".tel").val();
 but.mouseover(function(){
-  buttonhref.attr("href", "https://order.privator.club/aquarius.php?color="+ color +"&tel=" + tel;
+  buttonhref.attr("href", "https://order.privator.club/aquarius.php?color="+ color +"&tel=" + tel);
 });
 }
