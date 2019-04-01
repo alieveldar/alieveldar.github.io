136c10688b91db0fc41d5dbb6f2f9a420ac6349b
alieveldar <illuminatiday@gmail.com>
2019-04-01 14:16:45 +0300

    fixing

--- aquarius/css/order.js
+++ aquarius/css/order.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 var but = $(".t780__btn-wrapper");
-var buttonhref = $(".traiding");
+var buttonhref = $(".trading");
 var color = $(".t-product__option-select t-descr t-descr_xxs js-product-option-variants").val();
 var tel = $(".tel").val();
 buttonhref.mouseover(function(){
