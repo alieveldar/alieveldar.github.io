7f060eb5665bba3a264952e607cc715db71fde7c
alieveldar <illuminatiday@gmail.com>
2019-04-01 14:36:25 +0300

    fix select ta

--- aquarius/css/order.js
+++ aquarius/css/order.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 var but = $(".t780__btn-wrapper");
 var buttonhref = $(".trading");
-var color = $(".t-product__option-select t-descr t-descr_xxs js-product-option-variants").val();
+var color = $(".t-product__option-select t-descr t-descr_xxs js-product-option-variants option:selected").val();
 var tel = $(".tel").val();
 buttonhref.mouseover(function(){
   buttonhref.attr("href", "https://order.privator.club/aquarius.php?color="+ color +"&tel=" + tel);
